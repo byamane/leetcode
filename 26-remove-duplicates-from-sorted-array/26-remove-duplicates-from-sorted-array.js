356af573776572e1506a5aff870e2aae24700e07
Brandon Yamane <97767380+byamane@users.noreply.github.com>
2022-06-07 21:22:30 -0500

    Time: 678 ms (5.00%), Space: 45.8 MB (8.60%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function(nums) {
+    
+  for (let i = 0; i < nums.length; i++){
+    let temp = nums[i]
+    if (temp === nums[i+1]){
+      let idx = nums.indexOf(temp)
+      nums.splice(idx, 1, 101)
+    }
+  }
+    
+  if (nums.indexOf(101) === -1){
+    return nums.length
+  } else{
+    return nums.sort((a,b) => a-b).indexOf(101)
+  }
+};
