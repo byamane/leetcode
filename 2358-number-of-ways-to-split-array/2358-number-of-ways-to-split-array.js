3f10f337bb6dfd1f6b4900651052dabaa9765e2d
Brandon Yamane <97767380+byamane@users.noreply.github.com>
2025-03-01 15:08:13 -0600

    Time: 12 ms (61.94%), Space: 73.2 MB (8.37%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
@@ -4,20 +4,20 @@
  */
 var waysToSplitArray = function(nums) {
     // Time complexity: O(n^2) (two loops of nums.length --- look to refactor)
-    // Space complexity: O(n) (prefix)
+    // Space complexity: O(1)
 
-    let prefix = [nums[0]]
-    let splits = 0
+    let splits = 0, leftSection = 0, total = 0
 
-    for (let i=1; i < nums.length; i++) { 
-        prefix.push(nums[i] + prefix[prefix.length- 1])
+    for (const num of nums) {
+        total += num
     }
-    
-    for (let i=0; i < nums.length - 1; i++) {
-        let secondHalfSum = prefix[nums.length - 1] - prefix[i]
 
-        if (prefix[i] >= secondHalfSum) splits++
+    for (let i=0; i < nums.length - 1; i++) {
+        leftSection += nums[i]
+        let rightSection = total - leftSection
+        
+        if (leftSection >= rightSection) splits++
     }
 
     return splits
-};
+}
