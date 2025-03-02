629a236732dc178e9e6d3bc507d777f9a8a23faa
Brandon Yamane <97767380+byamane@users.noreply.github.com>
2025-03-02 09:29:24 -0600

    Time: 0 ms (100%), Space: 54.5 MB (12.84%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.js
@@ -3,11 +3,9 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let prefix = [nums[0]]
-
     for (let i=1; i < nums.length; i++){
-        prefix.push(nums[i] + prefix[i - 1])
+        nums[i] += nums[i-1]
     }
 
-    return prefix
+    return nums
 }
