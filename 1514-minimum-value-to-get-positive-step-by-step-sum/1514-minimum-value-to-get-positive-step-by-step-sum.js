e237303f4c2c607263e80204db188f862ca0a140
Brandon Yamane <97767380+byamane@users.noreply.github.com>
2025-03-03 17:38:40 -0600

    Time: 0 ms (100%), Space: 53.5 MB (11.76%) - LeetHub

--- 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.js
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/1514-minimum-value-to-get-positive-step-by-step-sum.js
@@ -4,18 +4,15 @@
  */
 var minStartValue = function(nums) {
     // Time complexity: O(n)
-    // Space complexity: O(n)
+    // Space complexity: O(1)
 
-    let prefix = [nums[0]]
+    let minimum = 0
+    let total = 0
 
-    for (let i=1; i < nums.length; i++){
-        prefix.push(nums[i] + prefix[i-1])
+    for (let i=0; i < nums.length; i++){
+        total += nums[i]
+        minimum = Math.min(minimum, total) // Having minimum set at 0 ensures for non-negative sums we return 1  
     }
 
-    if (Math.min(...prefix) < 1) return Math.abs(Math.min(...prefix)) + 1
-    else return 1
-
-    // console.log(Math.min(...prefix))
-    // return (Math.abs(Math.min(...prefix)) + 1)
-    // // return Math.abs(Math.min(...prefix)) + 1
+    return -minimum + 1
 };
