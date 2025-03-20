3db4555dcb6ef7ec02176983b5355dcc997011f2
Brandon Yamane <97767380+byamane@users.noreply.github.com>
2025-03-20 14:51:12 -0500

    Time: 1 ms (58.5%), Space: 52.4 MB (46.82%) - LeetHub

--- 1391-counting-elements/1391-counting-elements.js
+++ 1391-counting-elements/1391-counting-elements.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var countElements = function(arr) {
-    let ans = []
+    let ans = 0
     let set = new Set(arr)
     
     for (let i=0; i < arr.length; i++) {
-        if (set.has(arr[i]+1)) ans.push(arr[i])
+        if (set.has(arr[i]+1)) ans++
     }
     
-    return ans.length
+    return ans
 };
